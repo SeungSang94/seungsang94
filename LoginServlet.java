83928bb4e459cccbf8241a5179a8c68d8e98c7cf
SeungSang94 <164471110+SeungSang94@users.noreply.github.com>
2024-03-28 17:28:45 +0900

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 사용자가 입력한 로그인 정보를 받아오는 코드 작성
+        // 받아온 정보를 데이터베이스에서 확인하고 로그인 성공 또는 실패에 따라 적절한 응답을 보내는 코드 작성
+    }
+}
---
+++ SignupServlet.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class SignupServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 사용자가 입력한 회원가입 정보를 받아오는 코드 작성
+        // 받아온 정보를 데이터베이스에 저장하고 성공 또는 실패에 따라 적절한 응답을 보내는 코드 작성
+    }
+}
